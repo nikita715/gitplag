e19fe863c472b16443bc55688ae84afe9e0e2584
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-01 21:36:34 +0300

    test: moss and jplag analysis

---
+++ analysis/src/test/kotlin/ru/nikstep/redink/analysis/JPlagAnalyserTest.kt
@@ -0,0 +1,35 @@
+package ru.nikstep.redink.analysis
+
+import io.kotlintest.matchers.shouldEqual
+import org.junit.Test
+import ru.nikstep.redink.model.data.AnalysisResult
+
+class JPlagAnalyserTest {
+
+    val jPlagAnalysisService =
+        JPlagAnalyser(solutionStorageService, solutionsDir)
+
+    val expectedResult = listOf(
+        AnalysisResult(
+            students = "student2" to "student1",
+            countOfLines = -1,
+            percentage = 100,
+            repository = TEST_REPO_NAME,
+            fileName = TEST_FILE_NAME,
+            matchedLines = listOf((13 to 22) to (12 to 21))
+        ),
+        AnalysisResult(
+            students = "student3" to "student1",
+            countOfLines = -1,
+            percentage = 55,
+            repository = TEST_REPO_NAME,
+            fileName = TEST_FILE_NAME,
+            matchedLines = listOf((10 to 18) to (14 to 21))
+        )
+    )
+
+    @Test
+    fun analyse() {
+        jPlagAnalysisService.analyse(pullRequest) shouldEqual expectedResult
+    }
+}
---
+++ analysis/src/test/kotlin/ru/nikstep/redink/analysis/testData.kt
@@ -0,0 +1,41 @@
+package ru.nikstep.redink.analysis
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import ru.nikstep.redink.analysis.solutions.SolutionStorage
+import ru.nikstep.redink.model.entity.PullRequest
+import ru.nikstep.redink.util.Language
+import ru.nikstep.redink.util.asPath
+import java.nio.file.Paths
+
+const val TEST_REPO_NAME = "nikita715/plagiarism_test"
+const val TEST_FILE_NAME = "dir/FileTest.java"
+
+private val relSolutionsDir = asPath("src", "test", "resources", "test_solutions")
+
+internal val solutionsDir = Paths.get(relSolutionsDir).toFile().absolutePath
+
+private val base = Paths.get(relSolutionsDir, TEST_REPO_NAME, ".base", TEST_FILE_NAME).toFile()
+private val solution1 = Paths.get(relSolutionsDir, TEST_REPO_NAME, "student1", TEST_FILE_NAME).toFile()
+private val solution2 = Paths.get(relSolutionsDir, TEST_REPO_NAME, "student2", TEST_FILE_NAME).toFile()
+private val solution3 = Paths.get(relSolutionsDir, TEST_REPO_NAME, "student3", TEST_FILE_NAME).toFile()
+
+internal val testPreparedAnalysisFiles = PreparedAnalysisFiles(
+    TEST_REPO_NAME,
+    TEST_FILE_NAME,
+    Language.JAVA,
+    base,
+    listOf(solution1, solution2, solution3)
+)
+
+internal val solutionStorageService = mock<SolutionStorage> {
+    on { getCountOfSolutionFiles(TEST_REPO_NAME, TEST_FILE_NAME) } doReturn 3
+    on { loadAllBasesAndSolutions(any()) } doReturn listOf(testPreparedAnalysisFiles)
+}
+
+internal val pullRequest = mock<PullRequest> {
+    on { repoFullName } doReturn TEST_REPO_NAME
+    on { changedFiles } doReturn listOf(TEST_FILE_NAME)
+    on { creatorName } doReturn "student1"
+}
