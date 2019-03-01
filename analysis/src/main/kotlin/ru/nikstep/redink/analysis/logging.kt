d913c9a2203b818db9d3e6ef2c72c42c92a0c70f
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-02 01:12:29 +0300

    refactor: move log message to a separate file

--- analysis/src/main/kotlin/ru/nikstep/redink/analysis/logging.kt
+++ analysis/src/main/kotlin/ru/nikstep/redink/analysis/logging.kt
@@ -3,7 +3,6 @@ package ru.nikstep.redink.analysis
 import mu.KLogger
 import ru.nikstep.redink.model.entity.PullRequest
 
-
 internal fun KLogger.loggedAnalysis(pullRequest: PullRequest, action: () -> Unit) {
     info {
         "Analysis: start analysing of pr #${pullRequest.number}," +
@@ -14,4 +13,8 @@ internal fun KLogger.loggedAnalysis(pullRequest: PullRequest, action: () -> Unit
         "Analysis: complete analysing of pr #${pullRequest.number}," +
                 " repo ${pullRequest.repoFullName}, user ${pullRequest.creatorName}"
     }
+}
+
+internal fun KLogger.exceptionAtAnalysisOf(pullRequest: PullRequest) {
+    error { "Analysis: exception at the analysis of the pull request with id = ${pullRequest.id}\n" }
 }
