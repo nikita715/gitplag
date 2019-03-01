41a650c66c459b429c7fd225cd31679a685d7262
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-01 21:36:34 +0300

    test: moss and jplag analysis

---
+++ analysis/src/test/kotlin/ru/nikstep/redink/analysis/MossAnalyserTest.kt
@@ -0,0 +1,35 @@
+package ru.nikstep.redink.analysis
+
+import io.kotlintest.matchers.shouldEqual
+import org.junit.Test
+import ru.nikstep.redink.model.data.AnalysisResult
+
+class MossAnalyserTest {
+
+    val mossAnalysisService =
+        MossAnalyser(solutionStorageService, System.getenv("MOSS_ID"))
+
+    val expectedResult = listOf(
+        AnalysisResult(
+            students = "student1" to "student2",
+            countOfLines = 14,
+            percentage = 66,
+            repository = TEST_REPO_NAME,
+            fileName = TEST_FILE_NAME,
+            matchedLines = listOf((7 to 19) to (7 to 20))
+        ),
+        AnalysisResult(
+            students = "student1" to "student3",
+            countOfLines = 5,
+            percentage = 22,
+            repository = TEST_REPO_NAME,
+            fileName = TEST_FILE_NAME,
+            matchedLines = listOf((15 to 19) to (12 to 16))
+        )
+    )
+
+    @Test
+    fun analyse() {
+        mossAnalysisService.analyse(pullRequest) shouldEqual expectedResult
+    }
+}
