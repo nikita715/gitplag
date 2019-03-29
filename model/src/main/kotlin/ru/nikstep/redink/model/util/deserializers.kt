c40007dd6f1ea775106f4a02877d0305e3a56289
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-29 09:52:38 +0300

    chore: comments

--- model/src/main/kotlin/ru/nikstep/redink/model/util/deserializers.kt
+++ model/src/main/kotlin/ru/nikstep/redink/model/util/deserializers.kt
@@ -8,21 +8,33 @@ import ru.nikstep.redink.model.enums.AnalysisMode
 import ru.nikstep.redink.model.enums.GitProperty
 import ru.nikstep.redink.model.enums.Language
 
+/**
+* Jackson deserializer of [GitProperty]
+*/
 class GitPropertyDeserializer : JsonDeserializer<GitProperty>() {
     override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): GitProperty? =
         p?.valueAsString?.toUpperCase()?.let { GitProperty.valueOf(it) }
 }
 
+/**
+* Jackson deserializer of [Language]
+*/
 class LanguageDeserializer : JsonDeserializer<Language>() {
     override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): Language? =
         p?.valueAsString?.toUpperCase()?.let { Language.valueOf(it) }
 }
 
+/**
+* Jackson deserializer of [AnalysisMode]
+*/
 class AnalysisModeDeserializer : JsonDeserializer<AnalysisMode>() {
     override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): AnalysisMode? =
         p?.valueAsString?.toUpperCase()?.let { AnalysisMode.valueOf(it) }
 }
 
+/**
+* Jackson deserializer of [AnalyserProperty]
+*/
 class AnalyserPropertyDeserializer : JsonDeserializer<AnalyserProperty>() {
     override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): AnalyserProperty? =
         p?.valueAsString?.toUpperCase()?.let { AnalyserProperty.valueOf(it) }
