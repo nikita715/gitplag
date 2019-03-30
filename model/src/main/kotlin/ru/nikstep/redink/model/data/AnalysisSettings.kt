6d7b62879a7ccd35babbf75b47374fd099d69022
Nikita Stepochkin <nikstepbox@gmail.com>
2019-03-30 16:06:35 +0300

    refactor: AnalysisSettings builder

--- model/src/main/kotlin/ru/nikstep/redink/model/data/AnalysisSettings.kt
+++ model/src/main/kotlin/ru/nikstep/redink/model/data/AnalysisSettings.kt
@@ -10,8 +10,28 @@ import ru.nikstep.redink.model.enums.Language
  */
 class AnalysisSettings(
     val repository: Repository,
-    val branch: String,
-    val analyser: AnalyserProperty = repository.analyser,
-    val language: Language = repository.language,
-    val mode: AnalysisMode = repository.analysisMode
-)
+    val branch: String
+) {
+    var analyser: AnalyserProperty = repository.analyser
+        private set(value) {
+            field = value
+        }
+    var language: Language = repository.language
+        private set(value) {
+            field = value
+        }
+    var mode: AnalysisMode = repository.analysisMode
+        private set(value) {
+            field = value
+        }
+
+    constructor(
+        repository: Repository, branch: String, analyser: AnalyserProperty? = repository.analyser
+        , language: Language? = repository.language
+        , mode: AnalysisMode? = repository.analysisMode
+    ) : this(repository, branch) {
+        this.analyser = analyser ?: repository.analyser
+        this.language = language ?: repository.language
+        this.mode = mode ?: repository.analysisMode
+    }
+}
