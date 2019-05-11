56e4d889a8ca068ee9dd01a3ce6a265f976b6067
Nikita Stepochkin <nikstepbox@gmail.com>
2019-05-11 23:01:49 +0300

    chore: remove the docker be url arg

--- frontend/src/properties.js
+++ frontend/src/properties.js
@@ -1,3 +1,2 @@
 require("dotenv").config();
-export const serverUrl = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL
-  : "http://" + window.location.hostname + ":8090";
+export const serverUrl = "http://" + window.location.hostname + ":8090";
