a44835f768821416f82a09c78a8531682d5d68c8
Nikita Stepochkin <nikstepbox@gmail.com>
2019-05-13 01:13:16 +0300

    fix: path to analysis files

--- analysis/src/main/kotlin/io/gitplag/analysis/util.kt
+++ analysis/src/main/kotlin/io/gitplag/analysis/util.kt
@@ -5,7 +5,9 @@ import io.gitplag.model.entity.Analysis
 import java.time.LocalDateTime
 
 fun analysisFilesDirectoryName(analysisSettings: AnalysisSettings, date: LocalDateTime) =
-    analysisSettings.repository.name + "/" + date
+    analysisSettings.repository.name + "/" + date.toString().onlyDigits()
 
 fun analysisFilesDirectoryName(analysis: Analysis) =
-    analysis.repository.name + "/" + analysis.executionDate
+    analysis.repository.name + "/" + analysis.executionDate.toString().onlyDigits()
+
+private fun String.onlyDigits() = this.replace("[^\\d]*".toRegex(), "")
