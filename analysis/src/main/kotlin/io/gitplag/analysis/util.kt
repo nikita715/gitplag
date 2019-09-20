ddadff9222c52b1b73550d3924ad2a0887b994f6
Nikita Stepochkin <nikstepbox@gmail.com>
2019-09-20 23:06:24 +0300

    fix: Names of directories with analysis files

--- analysis/src/main/kotlin/io/gitplag/analysis/util.kt
+++ analysis/src/main/kotlin/io/gitplag/analysis/util.kt
@@ -3,26 +3,27 @@ package io.gitplag.analysis
 import io.gitplag.model.data.AnalysisSettings
 import io.gitplag.model.entity.Analysis
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 /**
  * Define path to source code files of the [analysisSettings]
  */
 fun analysisFilesDirectoryName(analysisSettings: AnalysisSettings) =
-    analysisSettings.repository.name + "/" + analysisSettings.executionDate.toString().onlyDigits()
+    analysisSettings.repository.name + "/" + analysisSettings.executionDate.toFormatOfFileName()
 
 /**
  * Define path to source code files of the [analysis]
  */
 fun analysisFilesDirectoryName(analysis: Analysis) =
-    analysis.repository.name + "/" + analysis.executionDate.toString().onlyDigits()
+    analysis.repository.name + "/" + analysis.executionDate.toFormatOfFileName()
 
 /**
  * Define path to source code files by [repoName] and [executionDate]
  */
 fun analysisFilesDirectoryName(repoName: String, executionDate: LocalDateTime) =
-    repoName + "/" + executionDate.toString().onlyDigits()
+    repoName + "/" + executionDate.toFormatOfFileName()
 
-private fun String.onlyDigits() = this.replace("[^\\d]*".toRegex(), "")
+private fun LocalDateTime.toFormatOfFileName() = toInstant(ZoneOffset.UTC).toEpochMilli().toString()
 
 /**
  * Info about the repo from the [analysisSettings]
